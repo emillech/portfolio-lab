883a8898e8b6299f9c73a2d748754903abddcf81
emil <contact@emillech.com.pl>
2021-01-13 11:00:39 +0100

    first view, configuration of htmls and static files in progress

--- project/project/urls.py
+++ project/project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from clothes_giver.views import LandingPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', LandingPage.as_view()),
 ]
