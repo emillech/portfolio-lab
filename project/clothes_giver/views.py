333bee9f0e82bcff53ea28c07172d780847b5c01
emil <contact@emillech.com.pl>
2021-01-13 15:14:16 +0100

    quantity and amount of org are now dynamic

--- project/clothes_giver/views.py
+++ project/clothes_giver/views.py
@@ -6,8 +6,17 @@ from clothes_giver.models import Category, Institution, Donation
 class LandingPage(View):
 
     def get(self, request):
+        quantity = 0
+        all_donations = Donation.objects.all()
+        for donation in all_donations:
+            quantity += donation.quantity
 
-        return render(request, 'index.html')
+        institutions = Institution.objects.count()
+        ctx = {
+            'quantity': quantity,
+            'institutions': institutions
+        }
+        return render(request, 'index.html', ctx)
 
 
 class AddDonation(View):
