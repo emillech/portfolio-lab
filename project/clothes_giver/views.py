9b790cc0d1d10c912964d3e54f9c67b213e90618
emil <contact@emillech.com.pl>
2021-01-13 19:00:15 +0100

    slider with companys is working, static js is ok

--- project/clothes_giver/views.py
+++ project/clothes_giver/views.py
@@ -11,10 +11,32 @@ class LandingPage(View):
         for donation in all_donations:
             quantity += donation.quantity
 
-        institutions = Institution.objects.count()
+        number_of_institutions = Institution.objects.count()
+
+        foundations = Institution.objects.filter(type=1)
+        foundations_data = {}
+        for foundation in foundations:
+            categories = Category.objects.filter(institution=foundation)
+            foundations_data.update({foundation: categories})
+
+        ngos = Institution.objects.filter(type=2)
+        ngos_data = {}
+        for ngo in ngos:
+            categories = Category.objects.filter(institution=ngo)
+            ngos_data.update({ngo: categories})
+
+        locals = Institution.objects.filter(type=3)
+        locals_data = {}
+        for local in locals:
+            categories = Category.objects.filter(institution=local)
+            locals_data.update({local: categories})
+
         ctx = {
             'quantity': quantity,
-            'institutions': institutions
+            'institutions': number_of_institutions,
+            'foundations': foundations_data,
+            'ngo': ngos_data,
+            'local': locals_data
         }
         return render(request, 'index.html', ctx)
 
