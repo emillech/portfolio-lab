3337e74f8f12b4eb2decb1400c3b0cc2b1286ce3
emil <contact@emillech.com.pl>
2021-01-13 12:25:02 +0100

    4 models added

--- project/clothes_giver/models.py
+++ project/clothes_giver/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+TYPES = (
+    (1, 'Fundation'),
+    (2, 'Non-governmental organization'),
+    (3, 'Local collection')
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=32)
+    description = models.TextField()
+    type = models.IntegerField(choices=TYPES, default=1)
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.IntegerField()
+    city = models.CharField(max_length=32)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.DateTimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=True,
+        default=None
+    )
+
+
